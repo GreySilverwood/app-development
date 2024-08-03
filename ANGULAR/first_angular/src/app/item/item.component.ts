b750b8a7dd2459d439f6719c47677a5f0dc45c93
Zaiqiang Li <zaiqiangli456@gmail.com>
2024-08-03 14:55:07 -0400

    August 3rd, 2024 - 0254PM Angular Component

---
+++ ANGULAR/first_angular/src/app/item/item.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.css'
+})
+export class ItemComponent {
+  username = "Peter Pan"
+
+  itemname = "Pencils"
+
+  setStatus:string = "No"
+
+  getStockStatus(){
+    return this.setStatus
+  }
+
+  isSoldOut:boolean = false
+
+  //set button to be disabled in 5 seconds
+  constructor(){
+    setTimeout(() => {
+      this.isSoldOut = true
+    }, 5000)
+  }
+
+  //Event Binding
+  onSave(){
+    alert('Information saved.')
+  }
+
+  productmsg:string = ""
+  total_in_stock = 20
+  total_added = 0
+  outStock = ""
+  btn_out_stock = false
+  addProduct(){
+    if(this.total_in_stock > 0){
+      this.total_added ++
+      this.total_in_stock --
+    }
+    else{
+      this.outStock = "Item is out of stock."
+      this.btn_out_stock = true
+    }
+  }
+  
+}
